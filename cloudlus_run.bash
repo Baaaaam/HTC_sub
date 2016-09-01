66055cbdda25309291034467ffd7c3d8cb2713ec
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-09-01 14:26:00 -0500

    toy for docker and cloudlus

---
+++ cloudlus_run.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#git clone https://github.com/Baaaaam/HTC_tool.git
+#cd HTC_tool
+#git fetch; git checkout docker
+#cd
+#./cloudlus -addr dory.fuelcycle.org:3030 work -interval 3s -whitelist cyclus cyan -timeout=3m 
